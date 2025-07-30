6f653650c0a91934fce8c8e19c844ac0127da6bb
MohanaSreenath <mohanasreenathk@gmail.com>
2025-07-30 17:06:30 +0530

    Register page Did Suceesfully finished still more tweaks we will do it later

--- Academic/src/main/java/com/example/Academic/Controller/RegisterController.java
+++ Academic/src/main/java/com/example/Academic/Controller/RegisterController.java
@@ -3,6 +3,7 @@ package com.example.Academic.Controller;
 
 //modules importing
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 //class importing
 import com.example.Academic.Services.RegisterService;
 
-@RestController
+@Controller
 public class RegisterController {
 
     @Autowired
@@ -21,10 +22,20 @@ public class RegisterController {
         return "registerpage"; // 👈 renders registerpage.html
     }
     @PostMapping("/register")
-    public String loadRegister(@RequestParam String name, @RequestParam String email, @RequestParam String password) {
-
-        boolean flag = registerService.registerUser(name, email, password);
-        return flag ? "loginpage2" : "registerpage";
+    public String loadRegister(
+            @RequestParam("name") String name,
+            @RequestParam("email") String email,
+            @RequestParam("password") String password
+    ) {
+        boolean flag = false;
+        try {
+        flag = registerService.registerUser(name, email, password);
+        }
+        catch (Exception e) {
+            System.out.println("Error in register service class");
+            return "Problem in the service class";
+        }
+        return flag ? "loginpage" : "registerpage";
     }
 
 }
